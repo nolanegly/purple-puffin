cec07624a7cedd37f8e5ccff1ac7dc591f6c244f
Nolan Egly <nolan@nolanegly.com>
2024-12-26 13:02:49 -0600

    Add two separate scenes
    Neither does anything interesting yet :)

--- src/PurplePuffin/PurplePuffinGame.cs
+++ src/PurplePuffin/PurplePuffinGame.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace PurplePuffin;
+
 
 public class PurplePuffinGame : Game
 {
@@ -10,13 +12,19 @@ public class PurplePuffinGame : Game
     private SpriteBatch _spriteBatch;
 
     private SpriteFont _arialFont;
-    private Vector2 _helloWorldPos;
+    private Vector2 _titlePos;
+    private Vector2 _gameplayPos;
+    
+    private SceneType _sceneType;
+    
 
     public PurplePuffinGame()
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
+
+        _sceneType = SceneType.Title;
     }
 
     protected override void Initialize()
@@ -33,7 +41,8 @@ public class PurplePuffinGame : Game
         _arialFont = Content.Load<SpriteFont>("arial");
         
         var viewport = GraphicsDevice.Viewport;
-        _helloWorldPos = new Vector2(viewport.Width / 2, viewport.Height / 2);
+        _titlePos = new Vector2(viewport.Width / 2, viewport.Height / 2);
+        _gameplayPos = new Vector2(viewport.Width / 2, viewport.Height / 2);
     }
 
     protected override void Update(GameTime gameTime)
@@ -42,7 +51,11 @@ public class PurplePuffinGame : Game
             Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
+        if (Keyboard.GetState().IsKeyDown(Keys.Enter))
+        {
+            _sceneType = SceneType.Game;
+        }
+        
 
         base.Update(gameTime);
     }
@@ -53,14 +66,34 @@ public class PurplePuffinGame : Game
 
         _spriteBatch.Begin();
         
-        // Draw Hello World message
-        var helloWorldMessage = "Hello World!";
-        var helloWorldOrigin = _arialFont.MeasureString(helloWorldMessage) / 2;
-        _spriteBatch.DrawString(_arialFont, helloWorldMessage, _helloWorldPos, Color.LightGreen,
-            0, helloWorldOrigin, 1.0f, SpriteEffects.None, 0.5f);
+        if (_sceneType == SceneType.Title)
+        {
+            var message = "Title scene";
+            var messageOrigin = _arialFont.MeasureString(message) / 2;
+            _spriteBatch.DrawString(_arialFont, message, _titlePos, Color.LightGreen,
+                0, messageOrigin, 1.0f, SpriteEffects.None, 0.5f);
+        }
+        else if (_sceneType == SceneType.Game)
+        {
+            // Draw game message
+            var message = "Game scene";
+            var messageOrigin = _arialFont.MeasureString(message) / 2;
+            _spriteBatch.DrawString(_arialFont, message, _titlePos, Color.LightGreen,
+                0, messageOrigin, 1.0f, SpriteEffects.None, 0.5f);
+        }
+        else
+        {
+            throw new Exception($"Unhandled Scene type: ${_sceneType}");
+        }
 
         _spriteBatch.End();
 
         base.Draw(gameTime);
     }
+}
+
+public enum SceneType
+{
+    Title,
+    Game
 }
